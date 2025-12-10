4d6645ae8ead5f3530e23ee3aacc1d5642cb90d1
Geovanny Mendoza <me@geovannycode.com>
2025-12-10 12:10:00 -0500

    Add code

--- shared/domain/src/main/kotlin/com/chatflow/shared/domain/ChatParticipant.kt
+++ shared/domain/src/main/kotlin/com/chatflow/shared/domain/ChatParticipant.kt
@@ -1,3 +1,11 @@
 package com.chatflow.shared.domain
 
-data class ChatParticipant()
+import java.time.Instant
+import java.util.UUID
+
+data class ChatParticipant(
+    val chatId: UUID,
+    val userId: UUID,
+    val role: ParticipantRole,
+    val joinedAt: Instant
+)
