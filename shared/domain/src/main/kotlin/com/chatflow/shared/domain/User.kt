f5e7911041df4ccb1639459c014fb0cec8eabff0
Geovanny Mendoza <me@geovannycode.com>
2025-12-13 12:10:00 -0500

    Add code

--- shared/domain/src/main/kotlin/com/chatflow/shared/domain/User.kt
+++ shared/domain/src/main/kotlin/com/chatflow/shared/domain/User.kt
@@ -1,3 +1,13 @@
 package com.chatflow.shared.domain
 
-data class User()
+import java.time.Instant
+import java.util.UUID
+
+data class User(
+    val id: UUID,
+    val email: String,
+    val name: String?,
+    val profilePictureUrl: String?,
+    val isVerified: Boolean,
+    val createdAt: Instant
+)
