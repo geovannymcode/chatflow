713c09e2cfb28f0a75172eb9f5fbf1a55bd493b4
Geovanny Mendoza <me@geovannycode.com>
2025-12-14 21:47:36 -0500

    Update UserEvent

--- shared/events/src/main/kotlin/com/chatflow/shared/events/UserEvent.kt
+++ shared/events/src/main/kotlin/com/chatflow/shared/events/UserEvent.kt
@@ -1,4 +1,21 @@
 package com.chatflow.shared.events
 
-class UserEvent {
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
+import java.time.Instant
+import java.util.UUID
+
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = UserRegisteredEvent::class, name = "USER_REGISTERED"),
+    JsonSubTypes.Type(value = UserVerifiedEvent::class, name = "USER_VERIFIED"),
+    JsonSubTypes.Type(value = PasswordResetRequestedEvent::class, name = "PASSWORD_RESET_REQUESTED")
+)
+sealed class UserEvent {
+    abstract val userId: UUID
+    abstract val occurredAt: Instant
 }
