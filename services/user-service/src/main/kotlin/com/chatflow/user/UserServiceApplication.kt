89584fe45a7a2e36e96ede825f518b8d55028fa9
Geovanny Mendoza <me@geovannycode.com>
2025-12-24 12:10:00 -0500

    Add code

--- services/user-service/src/main/kotlin/com/chatflow/user/UserServiceApplication.kt
+++ services/user-service/src/main/kotlin/com/chatflow/user/UserServiceApplication.kt
@@ -1,4 +1,11 @@
 package com.chatflow.user
 
-class UserServiceApplication {
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<UserServiceApplication>(*args)
 }
